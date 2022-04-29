5191f7488cf30af73910ed66508163706a52c60e
Victor Luiz da Silva <99228756+VictorLDS@users.noreply.github.com>
2022-04-29 09:26:37 -0300

    Add files via upload

---
+++ projetovg/src/App.vue
@@ -0,0 +1,103 @@
+<script></script>
+
+<template>
+  <nav>
+    <div id="titulo"><h1>Info NFL</h1></div>
+    <div id="qb"><h2>QB</h2></div>
+    <div id="rb"><h2>RB</h2></div>
+    <div id="wr"><h2>WR</h2></div>
+    <div id="ol"><h2>OL</h2></div>
+    <div id="te"><h2>TE</h2></div>
+  </nav>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=League+Gothic&display=swap");
+nav {
+  position: absolute;
+  width: 100%;
+  height: 200px;
+  left: 0px;
+  top: 12px;
+  color: white;
+
+  background-color: #000000;
+}
+
+#titulo {
+  position: absolute;
+  width: 270px;
+  height: 59px;
+  left: 78px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 70px;
+  color: #ffffff;
+}
+#qb {
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 440px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+  color: #fff;
+}
+#rb{
+
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 525px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+  color: #fff;
+}
+#wr{
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 603px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+}
+#ol{
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 688px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+}
+#te{
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 761px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+}
+</style>
