6ac54b600124f2b375a5f6349305644bcd8d90d6
Victor Luiz da Silva <99228756+VictorLDS@users.noreply.github.com>
2022-05-02 09:22:00 -0300

    Add files via upload

---
+++ projetointegrador-main/projetovg/src/App.vue
@@ -0,0 +1,187 @@
+<script></script>
+
+<template>
+  <nav>
+    <div id="titulo"><h1>Info NFL</h1></div>
+    <div id="qb"><h2>QB</h2></div>
+    <div id="rb"><h2>RB</h2></div>
+    <div id="wr"><h2>WR</h2></div>
+    <div id="ol"><h2>OL</h2></div>
+    <div id="te"><h2>TE</h2></div>
+    <div id="dl"><h2>DL</h2></div>
+    <div id="safety"><h2>S</h2></div>
+    <div id="edge"><h2>EDGE</h2></div>
+    <div id="lb"><h2>LB</h2></div>
+    <div id="cb"><h2>CB</h2></div>
+  </nav>
+<main>
+  <div id="txt"><p>Saiba as principais estatísticas dos melhores jogadores da NFL!</p>
+  <p>Rápido e fácil para quem está começando a acompanhar agora.</p>
+  <p>O Futebol Americano está crescendo sua comunidade no Brasil, e a Info NFL traz um jeito fácil de visualização das estatísticas do principais jogadores da NFL!</p>
+  </div>
+</main>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=League+Gothic&display=swap");
+nav {
+  position: absolute;
+  width: 100%;
+  height: 200px;
+  left: 0px;
+  top: 12px;
+  color: white;
+
+  background-color: #000000;
+}
+
+#titulo {
+  position: absolute;
+  width: 270px;
+  height: 59px;
+  left: 78px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 70px;
+  color: #ffffff;
+
+}
+#qb {
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 440px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+  color: #fff;
+}
+#rb{
+
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 525px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+  color: #fff;
+}
+#wr{
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 603px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+}
+#ol{
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 688px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+}
+#te{
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 761px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+}
+#dl{
+  position: absolute;
+  width: 89px;
+  height: 58px;
+  left: 949px;
+  top: 37px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 95px;
+}
+#safety{
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 1024px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+}
+#edge{
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 1085px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+}
+#lb{
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 1194px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+}
+#cb{
+  position: absolute;
+  width: 89px;
+  height: 59px;
+  left: 1288px;
+  top: 25px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 120px;
+}
+#txt{
+  position: absolute;
+  width: 1328px;
+  height: 250px;
+  left: 44px;
+  top: 605px;
+  font-family: "League Gothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 60px;
+}
+</style>
