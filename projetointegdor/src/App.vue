a181f48e836c037d83c89a3e2a056c63ba712e74
Victor Luiz da Silva <99228756+VictorLDS@users.noreply.github.com>
2022-05-05 15:04:18 -0300

    Add files via upload

---
+++ projetointegdor/src/App.vue
@@ -0,0 +1,32 @@
+<script></script>
+
+<template>
+  <nav>
+    <div id="titulo"><h1>Info NFL</h1></div>
+    <div id="qb"><h2>QB</h2></div>
+    <div id="rb"><h2>RB</h2></div>
+    <div id="wr"><h2>WR</h2></div>
+    <div id="ol"><h2>OL</h2></div>
+    <div id="te"><h2>TE</h2></div>
+    <div id="dl"><h2>DL</h2></div>
+    <div id="safety"><h2>S</h2></div>
+    <div id="edge"><h2>EDGE</h2></div>
+    <div id="lb"><h2>LB</h2></div>
+    <div id="cb"><h2>CB</h2></div>
+  </nav>
+  <main>
+    <img src="assets/lamarjackson.jpg" alt="" />
+    <div id="txt">
+      <p>Saiba as principais estatísticas dos melhores jogadores da NFL!</p>
+      <p>Rápido e fácil para quem está começando a acompanhar agora.</p>
+      <p>
+        O Futebol Americano está crescendo sua comunidade no Brasil, e a Info
+        NFL traz um jeito fácil de visualização das estatísticas do principais
+        jogadores da NFL!
+      </p>
+    </div>
+  </main>
+  <aside><h1>InfoNFL</h1></aside>
+</template>
+
+<style></style>
