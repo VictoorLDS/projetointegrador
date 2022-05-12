9e010abf813ab89f48f24f8bbb71e087c6d85bf2
Victor Luiz da Silva <99228756+VictorLDS@users.noreply.github.com>
2022-05-12 14:47:13 -0300

    Add files via upload

---
+++ projetointegdor/src/components/HomePage.vue
@@ -0,0 +1,32 @@
+<script></script>
+
+<template>
+  <nav>
+    <div id="titulo"><h1>Info NFL</h1></div>
+    <a href=""><div id="qb"><h2>QB</h2></div></a>
+    <a href=""><div id="rb"><h2>RB</h2></div></a>
+    <a href=""><div id="wr"><h2>WR</h2></div></a>
+    <a href=""><div id="ol"><h2>OL</h2></div></a>
+    <a href=""><div id="te"><h2>TE</h2></div></a>
+    <a href=""><div id="dl"><h2>DL</h2></div></a>
+    <a href=""><div id="safety"><h2>S</h2></div></a>
+    <a href=""><div id="edge"><h2>EDGE</h2></div></a>
+    <a href=""><div id="lb"><h2>LB</h2></div></a>
+    <a href=""><div id="cb"><h2>CB</h2></div></a>
+  </nav>
+  <main>
+    <img src="@/assets/nfl.jpg" alt="">
+    <div id="txt">
+      <p>Saiba as principais estatísticas dos melhores jogadores da NFL!</p>
+      <p>Rápido e fácil para quem está começando a acompanhar agora.</p>
+      <p>
+        O Futebol Americano está crescendo sua comunidade no Brasil, e a Info
+        NFL traz um jeito fácil de visualização das estatísticas do principais
+        jogadores da NFL!
+      </p>
+    </div>
+  </main>
+  <aside><h1>InfoNFL</h1></aside>
+</template>
+
+<style></style>
---
+++ projetointegdor/src/components/QbPage.vue
@@ -0,0 +1,101 @@
+<script></script>
+
+<template>
+  <nav>
+    <div id="titulo"><h1>Info NFL</h1></div>
+    <a href=""><div id="qb"><h2>QB</h2></div></a>
+    <a href=""><div id="rb"><h2>RB</h2></div></a>
+    <a href=""><div id="wr"><h2>WR</h2></div></a>
+    <a href=""><div id="ol"><h2>OL</h2></div></a>
+    <a href=""><div id="te"><h2>TE</h2></div></a>
+    <a href=""><div id="dl"><h2>DL</h2></div></a>
+    <a href=""><div id="safety"><h2>S</h2></div></a>
+    <a href=""><div id="edge"><h2>EDGE</h2></div></a>
+    <a href=""><div id="lb"><h2>LB</h2></div></a>
+    <a href=""><div id="cb"><h2>CB</h2></div></a>
+  </nav>
+  <main>
+      <div id="qbtitulo">QB</div>
+      <div id="quarterback">Quarterback</div>
+      <img src="/@assets/rodgersicon.jpg" alt="" height="185px" width="280px">
+      <div id="Arod">Aaron Rodgers</div>
+      <div id="Packers">Green Bay Packers</div>
+      <div class="altura">1,88m</div>
+      <div class="massa">102kg</div>
+      <div class="data">02/12/1983</div>
+
+
+      <img src="/@assets/joshallenicon.jpg" alt="" height="185px" width="280px">
+      <div id="Jallen">JoshAllen</div>
+      <div id="Bills">Buffalo Bills</div>
+      <div class="altura">1,96m</div>
+      <div class="massa">108kg</div>
+      <div class="data">21/05/1996</div>
+
+
+      <img src="/@assets/lamaricon.jpg" alt="" height="185px" width="280px">
+      <div id="lamar">Lamar Jackson</div>
+      <div id="Ravens">Baltimore Ravens</div>
+      <div class="altura">1,88m</div>
+      <div class="massa">96kg</div>
+      <div class="data">07/01/1997</div>
+
+      <img src="/@assets/bradyicon.jpg" alt="" height="185px" width="280px">
+      <div id="tb12">Tom Brady</div>
+      <div id="Bucs">Tampa Bay Buccanears</div>
+      <div class="altura">1,93m</div>
+      <div class="massa">102kg</div>
+      <div class="data">03/08/1977</div>
+
+      <img src="/@assets/wilsonicon.jpg" alt="" height="185px" width="280px">
+      <div id="wilson">Russell Wilson</div>
+      <div id="Broncos">Denver Broncos</div>
+      <div class="altura">1,80m</div>
+      <div class="massa">97kg</div>
+      <div class="data">29/11/1988</div>
+      <div class="stats"><table>
+        <tr>
+          <td>QbRtng</td>
+          <td>Tds</td>
+          <td>Yds</td>
+          <td>Ints</td>
+          <td>Fmbs</td>
+          <td>RYds</td>
+          <td>RTds</td>
+        </tr>
+      </table></div>
+  </main>
+</template>
+
+<style>
+#qbtitulo{
+    position: absolute;
+width: 132px;
+height: 92px;
+left: 96px;
+top: 193px;
+
+font-family: 'League Gothic';
+font-style: normal;
+font-weight: 400;
+font-size: 96px;
+line-height: 115px;
+
+color:#000;
+}
+#quarterback{
+    position: absolute;
+width: 352px;
+height: 46px;
+left: 254px;
+top: 222px;
+
+font-family: 'League Gothic';
+font-style: normal;
+font-weight: 400;
+font-size: 48px;
+line-height: 58px;
+
+color: #ACA7A7;
+}
+</style>
